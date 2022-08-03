36f14ec1c1bb4921ca383adf9f600adfcacef939
bumpsoo <bumpsoo063@protonmail.ch>
2022-08-03 16:35:40 +0900

    test needed,

--- ft_parse/ft_substitute.c
+++ ft_parse/ft_substitute.c
@@ -2,16 +2,13 @@
 #include "../libft/libft.h"
 
 
-static char	*ft_word(char *str)
+static char	*ft_word(char *str, char **key)
 {
 	char	*head;
 	int	i;
-	char	*ret;
 
 	i = 0;
 	++str;
-	if (*str == 0)
-		return (ft_strdup(""));
 	if (*str == BRACE_O)
 		str++;
 	head = str;
@@ -20,15 +17,35 @@ static char	*ft_word(char *str)
 		str++;
 		i++;
 	}
-	ret = ft_substr(head, 0, i);
+	*key = ft_substr(head, 0, i);
 	ft_check_error();
+	if (*str == BRACE_C)
+		str++;
+	return (str);
+}
+
+static char	*ft_union(char *left, char *key, char *right, t_info *info)
+{
+	char	*value;
+	char	*ret;
+	char	*temp;
+
+	value = ft_search_env(key, info);
+	ret = ft_strjoin(left, value);
+	temp = ret;
+	ret = ft_strjoin(ret, right);
+	free(temp);
+	free(left);
+	free(key);
+	free(right);
+	free(value);
 	return (ret);
 }
 
 char	*ft_substitue(char *str, t_info *info)
 {
 	char	*left;
-	char	*mid;
+	char	*key;
 	char	*right;
 	char	*temp;
 
@@ -36,7 +53,14 @@ char	*ft_substitue(char *str, t_info *info)
 	while (temp)
 	{
 		left = ft_substr(str, 0, temp - str - 1);
-		mid = ft_search(ft_word(temp), info);
+		ft_check_error();
+		temp = ft_word(temp, &key);
+		right = ft_strdup(temp);
+		ft_check_error();
+		temp = ft_union(left, key, right, info);
+		free(str);
+		str = temp;
 		temp = ft_strchr(str, DOLLAR);
 	}
+	return (str);
 }
