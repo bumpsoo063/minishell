1801ed75355b75f12d0b29f3dc3462fa025d28f3
bumpsoo <bumpsoo063@protonmail.ch>
2022-08-04 14:30:11 +0900

    t_queue -> str**

--- t_queue/t_queue.h
+++ t_queue/t_queue.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_queue.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bechoi <bechoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 14:29:33 by bechoi            #+#    #+#             */
+/*   Updated: 2022/08/04 14:29:33 by bechoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef T_QUEUE_H
 # define T_QUEUE_H
 
@@ -12,4 +24,5 @@ t_queue	*t_queue_new(char *str);
 void	t_queue_push(t_queue **head, t_queue *q);
 char	*t_queue_pop(t_queue **head);
 int	t_queue_size(t_queue *q);
+char	**t_queue_to_str(t_queue *q);
 #endif
