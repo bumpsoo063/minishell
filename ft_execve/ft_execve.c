344fe2423e3ef2cc4664822751c55c9a6efc6422
Kyungmin Yoon <kyoon@c8r6s14.42seoul.kr>
2022-08-12 01:56:45 +0900

    ft_execve 프로세스 나눠서 처리

--- ft_execve/ft_execve.c
+++ ft_execve/ft_execve.c
@@ -6,52 +6,73 @@
 /*   By: kyoon <kyoon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 13:09:02 by kyoon             #+#    #+#             */
-/*   Updated: 2022/08/10 21:49:01 by kyoon            ###   ########.fr       */
+/*   Updated: 2022/08/12 01:56:15 by kyoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_redirect.h"
+#include "ft_execve.h"
+
+static int	ft_process(char	**path, char **str)
+{
+	pid_t	pid;
+	int		i;
+	char	**cmd;
+	int		ret;
+	char	*tmp;
+
+	pid = fork();
+	if (pid < 0)
+		return (0);
+	if (pid == 0)
+	{
+		cmd = ft_calloc(sizeof(char *), 2);
+		i = 0;
+		while (i < 6)
+		{
+			tmp = cmd[0];
+			cmd[0] = ft_strjoin(path[i++], str[0]);
+			execve(cmd[0], str, NULL);
+		}
+		exit(1);
+	}
+	wait(&ret);
+	return (ret);
+}
+
+static int	ft_free(char **str, char **path)
+{
+	int	i;
+
+	i = 0;
+	if (path)
+		free(path);
+	if (str)
+	{
+		while (str[i])
+			free(str[i++]);
+		free(str);
+	}
+	return (0);
+}
 
 int	ft_execve(char *cmd)
 {
 	char	**path;
-	int		i;
 	char	**str;
-	char	*tmp;
-	int		flag;
+	int		ret;
 
-	str = ft_calloc(sizeof(char*), 2);
 	path = ft_calloc(sizeof(char*), 7);
-	flag = 0;
-	if (!path)
-		return (flag);
+	if (!str || !path)
+		return (0);
+	// PATH에서 받아와야함
 	path[0] = "/usr/local/bin/";
 	path[1] = "/usr/bin/";
 	path[2] = "/bin/";
 	path[3] = "/usr/sbin/";
 	path[4] = "sbin/";
 	path[5] = "/usr/local/munki";
-	i = 0;
-	while (!flag && i < 6)
-	{
-		str = ft_split(cmd, ' ');
-		tmp = str[0];
-		printf("%s\n", tmp);
-		str[0] = ft_strjoin(path[i++], str[0]);
-		printf("%s\n", tmp);
-		if (!execve(str[0], str, NULL))
-			flag = 1;
-		free(tmp);
-	}
-	free(str[0]);
-	free(str[1]);
-	free(str);
-	free(path);
-	return (flag);
-}
-
-int	main(int ac, char **av)
-{
-	ft_execve(av[1]);
-	return (0);
+	str = ft_split(cmd, ' ');
+	ret = ft_process(path, str);
+	ft_free(str, path);
+	return (ret);
 }
