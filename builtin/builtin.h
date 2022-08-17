b614e074328c9e5615011fd275d08ebb8ff4f24c
bumpsoo <bumpsoo063@protonmail.ch>
2022-08-17 16:27:13 +0900

    export 매개변수 없을 경우 구현

--- builtin/builtin.h
+++ builtin/builtin.h
@@ -6,6 +6,7 @@ void	ft_echo(char *str, int offset);
 void	ft_env(char **env);
 void	ft_exit(int status);
 char	**ft_export(char *str, char **env);
+void	ft_export_print(char **env, int size);
 int	ft_pwd(void);
 int	ft_unset(char *str, char **env);
 
