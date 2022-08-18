a08be71996e15fa8cab511c18884702379448dec
bumpsoo <bumpsoo063@protonmail.ch>
2022-08-18 21:27:09 +0900

    norm

--- minishell.h
+++ minishell.h
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bechoi <bechoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 21:26:15 by bechoi            #+#    #+#             */
+/*   Updated: 2022/08/18 21:26:44 by bechoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "const.h"
 # include <termios.h>
 
-typedef struct termios t_term;
+typedef struct termios	t_term;
 
 typedef struct s_info
 {
 	char	**env;
 	char	**parse;
-	int	in;
-	int	out;
-	int	exit;
+	int		in;
+	int		out;
+	int		exit;
 	t_term	org;
 }	t_info;
 
@@ -19,8 +31,8 @@ void	ft_check_error(void);
 t_info	ft_init_info(int argc, char **argv, char **env);
 void	ft_clean_fd(t_info *info);
 void	ft_clean_info(t_info *info, char *input);
-int	ft_exec(char **cmd ,t_info *info);
-int	ft_command(char **parse, t_info *info);
+int		ft_exec(char **cmd, t_info *info);
+int		ft_command(char **parse, t_info *info);
 void	ft_shift(char **parse, int p);
 
 #endif
